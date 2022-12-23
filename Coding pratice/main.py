d0fd2d39a80f0824595bac62e200333d41b27072
Robo1 <Flinnj77AF@github.com>
2022-12-23 13:53:56 -0700

    created train data

--- Coding pratice/main.py
+++ Coding pratice/main.py
@@ -7,30 +7,23 @@ Created on Fri Oct 22 07:12:49 2021
 
 import cv2
 import pytesseract as tes
-try:
-    from PIL import Image
-except ImportError:
-    import Image
-    
-
-tes.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 
 def main():
 
     
-    img_cv = cv2.imread(r'TechSmith-Blog-ExtractText.png')
+    img_cv = cv2.imread(r'test_bed_Press_start_K.png')
 
     # By default OpenCV stores images in BGR format and sincce pytesseract assumes RGB format,
     # we need to convert from BGR to RGB format/mode:
     img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
-    img = cv2.medianBlur(img_rgb, 5)
-    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    img = (255-img)
-    cv2.imshow('show', img)
+    #img = cv2.medianBlur(img_rgb, 5)
+    #img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    #img = (255-img)
+    cv2.imshow('show', img_rgb)
     cv2.waitKey(1)
-    txt = tes.image_to_string(img)
-    print(repr(txt))
+    txt = tes.image_to_string(img_rgb)
+    print(txt)
     input()
     
 if __name__ == "__main__":
